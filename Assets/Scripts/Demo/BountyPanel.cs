f40649c3ddd6fb76c0f4eb2872648d8ac04f7f36
Trickeri <troyvmaynard@gmail.com>
2023-05-24 17:45:55 -0400

    Fetching the player ID when referrals is opened

--- Assets/Scripts/Demo/BountyPanel.cs
+++ Assets/Scripts/Demo/BountyPanel.cs
@@ -11,20 +11,14 @@ public class BountyPanel : MonoBehaviour
 
     public IEnumerator LoadIconImage(string imageURL)
     {
-        print("Loading icon image " + imageURL);
         UnityWebRequest request = UnityWebRequestTexture.GetTexture(imageURL);
         yield return request.SendWebRequest();
 
         if (request.responseCode == 200)
         {
             Texture2D myTexture = ((DownloadHandlerTexture)request.downloadHandler).texture;
-            // bountyIconImage.rectTransform.rect
             Sprite newSprite = Sprite.Create(myTexture, new Rect(0, 0, myTexture.width, myTexture.height), new Vector2(0.5f, 0.5f));
             bountyIconImage.sprite = newSprite;
-        }
-        else
-        {
-
         }
     }
 }
--- Assets/Scripts/Demo/UI_Referrals.cs
+++ Assets/Scripts/Demo/UI_Referrals.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+
 using GetReferralInfoByPlayerIdRequestParams = NexusSDK.ReferralsAPI.GetReferralInfoByPlayerIdRequestParams;
 using GetReferralInfoByPlayerIdResponseCallbacks = NexusSDK.ReferralsAPI.GetReferralInfoByPlayerIdResponseCallbacks;
 using GetReferralInfoByPlayerId200Response = NexusSDK.ReferralsAPI.GetReferralInfoByPlayerId200Response;
 using Referral = NexusSDK.ReferralsAPI.Referral;
 using ReferralError = NexusSDK.ReferralsAPI.ReferralError;
+
+using GetCreatorsRequestParams = NexusSDK.AttributionAPI.GetCreatorsRequestParams;
+using GetCreators200Response = NexusSDK.AttributionAPI.GetCreators200Response;
+using Creator = NexusSDK.AttributionAPI.Creator;
 
 
 
@@ -22,20 +27,39 @@ public class UI_Referrals : MonoBehaviour
     {
         CopyCodeBtn.onClick.AddListener(() => HandleCopyCodeButtonClick());
 
-        GetReferralInfoByPlayerIdRequestParams requestParams = new GetReferralInfoByPlayerIdRequestParams();
-        requestParams.playerId = "0Con-WN07cuBMdEkr56oo";
-        requestParams.groupId = "";
+        GetCreatorsRequestParams requestParams = new GetCreatorsRequestParams();
         requestParams.page = 1;
-        requestParams.pageSize = 10;
-        requestParams.excludeReferralList = true;
-
-        StartCoroutine(NexusSDK.ReferralsAPI.StartGetReferralInfoByPlayerIdRequest(requestParams, 
-            new GetReferralInfoByPlayerIdResponseCallbacks () {OnGetReferralInfoByPlayerId200Response = OnGetReferralInfoByPlayerId200ResponseFunction, OnGetReferralInfoByPlayerId400Response = OnGetReferralInfoByPlayerId400ResponseFunction}, 
-            ErrorCallbackFunction));
+        requestParams.pageSize = 99;
+        requestParams.groupId = "";
+        StartCoroutine(NexusSDK.AttributionAPI.StartGetCreatorsRequest(requestParams, 
+            OnGetCreators200ResponseFunction, ErrorCallbackFunction));
     }
     void OnDisable()
     {
         CopyCodeBtn.onClick.RemoveAllListeners();
+    }
+
+
+    void OnGetCreators200ResponseFunction(GetCreators200Response Response)
+    {
+        print("Get Creators 200 response");
+
+        foreach (Creator creator in Response.creators)
+        {
+            if (creator.name == localUsername.text)
+            {
+                GetReferralInfoByPlayerIdRequestParams requestParams2 = new GetReferralInfoByPlayerIdRequestParams();
+                requestParams2.playerId = creator.id;
+                requestParams2.groupId = "";
+                requestParams2.page = 1;
+                requestParams2.pageSize = 10;
+                requestParams2.excludeReferralList = true;
+
+                StartCoroutine(NexusSDK.ReferralsAPI.StartGetReferralInfoByPlayerIdRequest(requestParams2, 
+                    new GetReferralInfoByPlayerIdResponseCallbacks () {OnGetReferralInfoByPlayerId200Response = OnGetReferralInfoByPlayerId200ResponseFunction, OnGetReferralInfoByPlayerId400Response = OnGetReferralInfoByPlayerId400ResponseFunction}, 
+                    ErrorCallbackFunction));
+            }
+        }
     }
 
 
